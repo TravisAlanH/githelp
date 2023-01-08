417d50a71ef9692d42780c67804a9d12c250bcca
TravisAlanH <travis.Heidelberger@trojans.dsu.edu>
2023-01-08 17:18:57 -0600

    Revert "Revert "Router Paths / DataBase for Project""
    This reverts commit 555126f37d2b5404df8350427c6aa9177f6f6451.

---
+++ src/Comp/Nav.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import data from "../PageData";
+
+// const Links = [
+//   {
+//     "Name": "Get Clone",
+//     "Link": "/clone",
+//   },
+//   {
+//     "Name": "Get Branch",
+//     "Link": "/branch",
+//   },
+// ];
+
+export default function Nav() {
+  return (
+    <div className="fixed w-12 h-screen flex flex-col gap-8 justify-between items-center py-20 bg-teal-800 lg:flex-row lg:gap-4 lg:h-12 lg:w-screen lg:px-20 lg:py-0">
+      {data.NavLinks.map((item, index) => {
+        return (
+          <div className="-rotate-90 lg:rotate-0 w-24" key={index}>
+            <Link to={item.Link} className="NavListItemLink">
+              {item.Name}
+            </Link>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
